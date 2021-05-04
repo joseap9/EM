8085a6f6226f3f5cbdaa84f0cf4fcdd0381f174f
joseap9 <joseperdomo2428@gmail.com>
2021-05-04 08:54:30 -0400

    act

--- EMR/Listas.py
+++
@@ -1,8 +0,0 @@
-lista = []
-def __init__(self, lista=[]):
-    self.medicohospital = lista
-def agregar(self, medico):
-    self.listamedicos.append(medico)
-def mostrar(self):
-    for medico in self.lista:
-        print(medico)
