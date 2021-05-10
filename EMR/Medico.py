66c1979f6318ed9dca30c7303c575fcf5667c013
joseap9 <joseperdomo2428@gmail.com>
2021-05-10 13:22:34 -0400

    act

--- EMR/Medico.py
+++ EMR/Medico.py
@@ -1,16 +1,25 @@
 from Persona import *
+from Especialidad import *
+from Paciente import *
+import tkinter as tk
 
 
-class Medico( Persona ):
+class Medico( Persona,Especialidad ):
 
-    def __init__(self,idMedico):
+    def __init__(self,nombre,rut,sexo,direccion,idEspecialista,nombreEspecialidad,cantidadEspecialistas,idMedico):
+
+        Persona.__init__(self,nombre,rut,sexo,direccion)
+        Especialidad.__init__(self,idEspecialista,nombreEspecialidad,cantidadEspecialistas)
         self.idMedico=idMedico
 
 
-    def getidMedico(self):
+
+
+
+    def getIdMedico(self):
         return self.idMedico
 
-    def setidMedico(self,idMedico):
+    def setIdMedico(self,idMedico):
         self.idMedico=idMedico
 
 
@@ -35,3 +44,65 @@ class Medico( Persona ):
     #deveria ser una funcion y no un contenido de la clase
     #def mostrarMedico(self,listadoDeMedicos):
         #buscar medico por el rut del medico
+
+    def ventanaMedicoEsp(self):
+
+        def close_window(root):
+            root.destroy()
+        
+        root = tk.Tk()
+        root.title("Medico Especialista")
+        root.geometry("550x500")
+
+        #labels
+
+        tk.Label(root, text="filtrar ", font=("Aharoni",10,'bold')).place(x=40,y=30)
+        buscaP= tk.StringVar()
+        tk.Radiobutton(root, value="Traumatologia", variable=buscaP, text="Traumatologia").place(x=100,y=30)
+        tk.Radiobutton(root, value="neurologia", variable=buscaP, text="neurologia").place(x=220,y=30)
+        tk.Radiobutton(root, value="cardiologia", variable=buscaP, text="cardiologia").place(x=320,y=30)
+        tk.Button(root, text="ok", width=5).place(x=450,y=30)
+
+        tk.Label(root, text="Nombre:").place(x=40, y=100)
+        nombre = tk.Entry(root, width=40,state="readonly")
+        nombre.focus()
+        nombre.place(x=170,y=100)
+        
+
+
+        tk.Label(root, text="Rut:").place(x=40,y=130)
+        rut = tk.Entry(root, width=40,state="readonly")
+        rut.place(x=170,y=130)
+
+
+
+        tk.Label(root, text="Diagnostico Inicial:").place(x=40,y=160)
+        diagI = tk.Entry(root, width=40,state="readonly")
+        diagI.place(x=170,y=160)
+
+        tk.Label(root, text="Estado:").place(x=40,y=190)
+        estado = tk.Entry(root, width=40,state="readonly")
+        estado.place(x=170,y=190)
+
+        tk.Label(root, text="Diagnostico:").place(x=40,y=250)
+        diagnostico = tk.Entry(root, width=40)
+        diagnostico.place(x=170,y=250)
+
+        tk.Label(root, text="Tratamiento:").place(x=40,y=280)
+        tratamiento =  tk.Text(root, height = 5, width= 25, padx=15,pady=15)
+        tratamiento.place(x=170,y=280)
+
+        tk.Button(root, text = "llamar autoridades",width = 15).place(x=77, y=425)
+
+        tk.Button(root, text = "alta medica",width = 10, command = lambda: close_window(root) ).place(x=250, y=425)
+
+        tk.Button(root, text = "siguiente",width = 10).place(x=400, y=425)
+
+        
+        
+        
+
+
+        
+
+        root.mainloop()
--- EMR/Paciente.py
+++ EMR/Paciente.py
@@ -1,22 +1,54 @@
 from Persona import *
+import tkinter as tk
 
 listaPacientes = []
 
 class Paciente(Persona):
 
-    def __init__(self,nombre,rut,sexo,direccion,diagnosticoI,estado,camaAsig,numeroDeAtencion):
+    def __init__(self,nombre,rut,sexo,direccion,diagnosticoI,estado,despacho,camaAsig,numeroDeAtencion):
         super().__init__(nombre,rut,sexo,direccion)
         self.estado = estado
         self.camaAsig = camaAsig
         self.numeroDeAtencion = numeroDeAtencion
         self.diagnosticoI=diagnosticoI
-        # self.medicoTratante= #medico especialista podemos sacarlo del area y buscar el primer
-        # medico que posea esta, deberia ser un afuncion la cual ejecutemos aqui
-        # self.medicoPrioridad=#primer medico que lo atiende
+        self.despacho = despacho
+        
 
+    def getEstado(self):
+        return self.estado
 
-    def solicitarAtencion(self,nombre,rut,sexo,direccion,diagnosticoI, estado, camaAsig, numAtencion):
-        nuevoPaciente = Paciente(nombre,rut,sexo,direccion,diagnosticoI,estado,camaAsig,numAtencion)
+    def getCamaAsig(self):
+        return self.camaAsig
+
+    def getNumeroDeAtencion(self):
+        return self.numeroDeAtencion
+    
+    def getDespacho(self):
+        return self.despacho
+
+    def getDiagnosticoI(self):
+        return self.diagnosticoI
+    
+
+    def setEstado(self, estado):
+        self.estado = estado
+
+    def setCamaAsig(self, camaAsig):
+        self.camaAsig = camaAsig
+
+    def setNumeroDeAtencion(self, n):
+        self.numeroDeAtencion = n
+
+    def setDiagnosticoI(self,d):
+        self.diagnosticoI = d
+    
+    def setDespacho(self, des):
+        self.despacho = des
+
+    
+    
+    def solicitarAtencion(self,nombre,rut,sexo,direccion,diagnosticoI, estado, despacho, camaAsig, numAtencion):
+        nuevoPaciente = Paciente(nombre,rut,sexo,direccion,diagnosticoI,estado,despacho,camaAsig,numAtencion)
         listaPacientes.append(nuevoPaciente)
 
         n = 1
@@ -30,64 +62,97 @@ class Paciente(Persona):
         for x in range(len(listaPacientes)):
             print(listaPacientes[x].__str__())
 
+
+
+    def buscaPorRut(self,rut):
+        
+        for x in range(len(listaPacientes)):
+            if listaPacientes[x].getRut == rut:
+                print("TEST")
+            else:
+                print("FAIL")
+
     def __str__(self):
-        return super().__str__() + "\n"  + "diagnostico Inicial: " + self.diagnosticoI + "\n" + "estado: "+ self.estado + "\n" + "num de atencion" + str(self.numeroDeAtencion) + "\n" + "cama asig" +str(self.camaAsig)
+        return super().__str__() + "\n"  + "diagnostico Inicial: " + self.diagnosticoI + "\n" + "estado: "+ self.estado + "\n" + "despacho: "+ self.despacho+"\n" + "num de atencion" + str(self.numeroDeAtencion) + "\n" + "cama asig" +str(self.camaAsig)+"\n======================="
+    
+    
+    def ventanaAtencion(self):
 
-    def getestado(self):
-        return self.estado
+        def close_window(root):
+            root.destroy()
 
-    def getcamaAsig(self):
-        return self.camaAsig
+        sexos= ["masculino","femenino","otro"]
+        estados=["reanimacion","emergencia","urgencia","prioritario","no urgencia"]
+        #despachos=["traumatologia","neurologia","cardiologia"]
+        root = tk.Tk()
+        root.title("Solicitar Atencion")
+        root.geometry("450x350")
 
-    def getnumeroDeAtencion(self):
-        return self.numeroDeAtencion
+        #labels
 
-    def getmedicoTratante(self):
-        return self.medicoTratante
+        tk.Label(root, text="Nombre:").place(x=40, y=30)
+        nombre = tk.Entry(root, width=40)
+        nombre.focus()
+        nombre.place(x=170,y=30)
+        
 
-    def getmedicoPrioridad(self):
-        return self.medicoPrioridad
 
-    def setestado(self, estado):
-        self.estado = estado
+        tk.Label(root, text="rut:").place(x=40,y=60)
+        rut = tk.Entry(root, width=40)
+        rut.place(x=170,y=60)
 
-    def setcamaAsig(self, camaAsig):
-        self.camaAsig = camaAsig
+        tk.Label(root, text="sexo:").place(x=40,y=90)
+        sexo= tk.Spinbox(root, values=sexos)
+        sexo.place(x=170,y=90)
 
-    def setnumeroDeAtencion(self, n):
-        self.numeroDeAtencion = n
 
-    def setmedicoTratante(self, medico):
-        self.medicoTratante = medico
+        tk.Label(root, text="direccion:").place(x=40,y=120)
+        direccion = tk.Entry(root, width=40)
+        direccion.place(x=170,y=120)
 
-    def setmedicoPrioridad(self, prioridad):
-        self.medicoPrioridad = prioridad
+        #tk.Label(root, text="cama Asignada:").grid(pady=5, row=4, column=0)
+        #camaAsig = tk.Entry(root, width=40)
+        #camaAsig.grid(padx=5, row=4, column=1)
 
-    def asigPrioridadPaciente(self, listadoDePacientes, prioridad):
-        if prioridad == 1:
-            # agregar a listadoDePacientes
-            print("")
-        elif prioridad == 2:
-            # agregar a listadoDePacientes
-            print("")
-        elif prioridad == 3:
-            # agregar a listadoDePacientes
-            print("")
+        tk.Label(root, text="Diagnostico Inicial:").place(x=40,y=150)
+        diagI = tk.Entry(root, width=40)
+        diagI.place(x=170,y=150)
 
-    def estadoPaciente(self):
-        # mostrar si se encuentra internado o no (puede ser comparardo su rut con la lista de pacientes y si pocee cama)
-        # asi identificamos si esta en espera, tratamiento o sin ser atendido
-        print("")
+        tk.Label(root, text="estado:").place(x=40,y=180)
+        estado = tk.Spinbox(root, values=estados)
+        estado.place(x=170,y=180)
 
-    def mostrarPacienteEspecialidad(self):
-        # no entiendo que hace esta funcion....
-        print("")
+        tk.Label(root, text="despacho:").place(x=40,y=210)
+        despacho= tk.StringVar()
+        tk.Radiobutton(root, variable=despacho, text="Traumatologia",value="Traumatologia").place(x=170,y=210)
+        tk.Radiobutton(root, variable=despacho, text="neurologia",value="neurologia").place(x=170,y=230)
+        tk.Radiobutton(root, variable=despacho, text="cardiologia", value="cardiologia").place(x=170,y=250)
+        
 
-    def dashboardPacientes(self, listadoDePacientes):
-        # for i=0;i++;i=lend(listadoDePacientes):
-        # print(listadoDePacientes[i])
-        print("")
+        #tk.Label(root, text="numero de atencion:").grid(pady=5, row=6, column=0)
+        #numAtencion = tk.Entry(root, width=40)
+        #numAtencion.grid(padx=5, row=6, column=1)
 
-    def despacharEspecialidad(self):
-        # nose si espara llevarlo a un area o sacarlo de ahi
-        print("")
+        agregar = tk.Button(root, text="Agregar", width=50, command=lambda: [self.solicitarAtencion(nombre.get(),rut.get(),sexo.get(),direccion.get(),diagI.get(),estado.get(),despacho.get(),"",""), self.muestra(),close_window(root)])
+        agregar.place(x=50,y=300)
+
+        #mostrar = tk.Button(root, text="Listado de Pacientes", width=50, command=lambda: btnMostrar())
+        #mostrar.grid(padx=10, pady=10, row=7, column=0, columnspan=2)
+
+        root.mainloop()
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    
